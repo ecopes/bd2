3cdd088511bba6f38ca023f8041ce15185c1ad19
ecopes <emacopes@gmail.com>
2017-04-20 14:08:14 -0300

    se agregaron algunos servicios y el esqueleto de los form

--- src/main/java/bd2/web/MuberRestController.java
+++ src/main/java/bd2/web/MuberRestController.java
@@ -10,9 +10,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -42,14 +42,45 @@ public class MuberRestController {
 	@RequestMapping(value = "/listarWS", method = RequestMethod.GET)
 	public String example() {
 		String pagina = "<!DOCTYPE html><html><head><title>Listado de los ws</title></head><body>"
-				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/pasajeros'>listar todos los pasajeros</a><br>"
-				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/conductores'>listar todos los conductores</a><br>"
-				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/viajes/abiertos'>listar todos los viajes abiertos</a><br>"
-				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/conductores/detalle/5'>listar detalle conductor id 5</a><br>"
+				+"<br><h3>Listar todos los pasajeros registrados en Muber:</h3>"
+				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/pasajeros'>Listar</a><br>"
+
+				+"<br><h3>Listar todos los conductores registrados en Muber:</h3>"
+				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/conductores'>Listar</a><br>"
+
+				+"<br><h3>Listar todos los viajes abiertos en Muber:</h3>"
+				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/viajes/abiertos'>Listar</a><br>"
+
+				+"<br><h3>Obtener la información de un conductor:</h3>"
+				+ "<form action='http://127.0.0.1:8080/MuberRESTful/rest/services/conductores/detalle/' method='get'>"
+				+ "<input type='text' name='conductorId' placeholder='ID del conductor' required><br>"
+				+ "<input type='submit' value='Enviar'></form> "
+
+				+ "<br><h3>Crear Viaje:</h3>"
+				+ "<form action='http://127.0.0.1:8080/MuberRESTful/rest/services/----------TODO----------' method='post'>"
+				+ "<input type='text' name='origen' placeholder='Origen' required><br>"
+				+ "<input type='text' name='destino' placeholder='Destino' required><br>"
+				+ "<input type='number' name='conductorId' placeholder='ID del conductor' required><br>"
+				+ "<input type='number' step='0.01' name='costoTotal' placeholder='Costo total' required><br>"
+				+ "<input type='number' name='cantidadPasajeros' placeholder='Cantidad de pasajeros' required><br>"
+				+ "<input type='submit' value='Enviar'></form> "
+
+				+ "<br><h3>Crear una calificación de un pasajero para un viaje en particular:</h3>"
+				+ "<form action='http://127.0.0.1:8080/MuberRESTful/rest/services/----------TODO----------' method='post'>"
+				+ "<input type='number' name='viajeId' placeholder='ID del Viaje' required><br>"
+				+ "<input type='number' name='pasajeroId' placeholder='ID del Pasajero' required><br>"
+				+ "<input type='number' step='0.01' name='puntaje' placeholder='Puntaje' required><br>"
+				+ "<input type='text' name='comentario' placeholder='Comentario' required><br>"
+				+ "<input type='submit' value='Enviar'></form> "
+				
+				+"<br><h3>Listar los 10 conductores mejor calificados que no tengan viajes abiertos registrados:</h3>"
+				+"<a href='http://127.0.0.1:8080/MuberRESTful/rest/services/----------TODO----------'>Listar</a><br>"
+
 				+"</body><html>";
+		
 		return pagina;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/pasajeros", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
 	public String pasajeros() {
@@ -64,7 +95,7 @@ public class MuberRestController {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(aMap);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/conductores", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
 	public String conductores() {
@@ -79,7 +110,7 @@ public class MuberRestController {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(aMap);
 	}
-	
+
 	@RequestMapping(value = "/viajes/abiertos", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
 	public String viajesAbiertos() {
 		Query query = getSession().createQuery("from Viaje where finalizado = false");
@@ -100,9 +131,9 @@ public class MuberRestController {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(aMap);
 	}
-	
-	@RequestMapping(value = "/conductores/detalle/{conductorId}", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
-	public String conductorDetalle(@PathVariable(value="conductorId") int id) {
+
+	@RequestMapping(value = "/conductores/detalle/", method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
+	public String conductorDetalle(@RequestParam(value="conductorId") int id) {
 		Conductor conductor = (Conductor) getSession().get(Conductor.class,id);
 		Map<String, Object> aMap = new HashMap<String, Object>();
 		aMap.put("Nombre de Usuario", conductor.getNombre());
@@ -112,6 +143,6 @@ public class MuberRestController {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(aMap);
 	}
-	
-	
+
+
 }
